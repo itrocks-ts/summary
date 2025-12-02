1fbc050c4e97eb8d9d047d10dde241312be5ae44
Baptiste Pillot <baptiste@pillot.fr>
2025-12-02 12:16:22 +0100

    Summarize with representativeValueOf + sort

--- src/summary.ts
+++ src/summary.ts
@@ -1,9 +1,11 @@
-import { Action }     from '@itrocks/action'
-import { Need }       from '@itrocks/action'
-import { Request }    from '@itrocks/action-request'
-import { Route }      from '@itrocks/route'
-import { dataSource } from '@itrocks/storage'
-import { Identifier } from '@itrocks/storage'
+import { Action }                from '@itrocks/action'
+import { Need }                  from '@itrocks/action'
+import { Request }               from '@itrocks/action-request'
+import { representativeValueOf } from '@itrocks/class-view'
+import { Route }                 from '@itrocks/route'
+import { dataSource }            from '@itrocks/storage'
+import { Identifier }            from '@itrocks/storage'
+import { Sort }                  from '@itrocks/storage'
 
 export type SummaryRecord = [Identifier, string]
 
@@ -14,7 +16,13 @@ export class Summary<T extends object = object> extends Action<T>
 
 	async json(request: Request<T>)
 	{
-		const summary = (await dataSource().search(request.type)).map(object => [object.id, object + ''])
+		const summary = await Promise.all(
+			(await dataSource().readAll(request.type, Sort))
+				.map(async object => [
+					object.id,
+					await representativeValueOf(object)
+				])
+		)
 		return this.jsonResponse(summary)
 	}
 
